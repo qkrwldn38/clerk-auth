2e85939c2f44ed667f58b0e402a8359f83014c89
qkrwldn38 <qkrwldn751@gmail.com>
2023-09-13 10:03:29 +0900

    2nd

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-5">Dashboard</h1>
+      <p className="mb-5">Welcom 안녕</p>
+    </div>
+  )
+}
+
+export default DashboardPage
---
+++ app/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,9 @@
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+const SingUpPage = () => {
+  return (
+    <SignUp/>
+}
+
+export default SingUpPage
