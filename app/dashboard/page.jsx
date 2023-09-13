a621b587e648d970f3b804b1ca47bb725ec214fb
qkrwldn38 <qkrwldn751@gmail.com>
2023-09-13 10:08:12 +0900

    3rd

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -4,7 +4,13 @@ const DashboardPage = () => {
   return (
     <div>
       <h1 className="text-2xl font-bold mb-5">Dashboard</h1>
-      <p className="mb-5">Welcom 안녕</p>
+      <p className="mb-5">
+        C'mon C'mon 마산스트리트여 커먼커먼커먼 나의 친구여 반만년 우뚝 설 우리
+        샛별이여 서울 히어로즈는 전진한다 벗이여 친구여 나의 자랑이여 서울
+        히어로즈는 승리한다 숨가쁘게 달려온 어제의 우리 이제 오늘의 얼굴을 닦자
+        다시 외치자 소리치자 손ㅇㄹ 잡아라 잔을 채워라 오랜갈증 한강물을 싹
+        비웠다 거침넚는 서울 히어로~~
+      </p>
     </div>
   )
 }
--- app/sign-up/[[...sign-up]]/page.jsx
+++ app/sign-up/[[...sign-up]]/page.jsx
@@ -2,8 +2,7 @@ import { SignUp } from '@clerk/nextjs'
 import React from 'react'
 
 const SingUpPage = () => {
-  return (
-    <SignUp/>
+  return <SignUp />
 }
 
 export default SingUpPage
